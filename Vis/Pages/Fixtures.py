c47416686a0c3c7b3175684741ab0f97f392a7c3
Wael Hcine <wael.hcine@esprit.tn>
2024-10-17 00:44:22 +0100

    awawawa

--- Vis/Pages/Fixtures.py
+++ Vis/Pages/Fixtures.py
@@ -12,21 +12,21 @@ fpl_utils_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '
 print(fpl_utils_path)
 sys.path.append(fpl_utils_path)
 
-# Use relative imports for better clarity
-from .fpl_api_collection import (
+# Use absolute imports
+from fpl_utils.fpl_api_collection import (
     get_bootstrap_data,
     get_current_gw,
     get_fixt_dfs,
     get_league_table
 )
-from .fpl_utils import (
+from fpl_utils.fpl_utils import (
     define_sidebar,
     get_annot_size,
     map_float_to_color,
     get_text_color_from_hash,
     get_rotation
 )
-from .fpl_params import (
+from fpl_utils.fpl_params import (
     TIMEZONES_BY_CONTINENT,
     AUTHOR_CONTINENT,
     AUTHOR_CITY
