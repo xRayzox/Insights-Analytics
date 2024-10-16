f0645b66738355d088cebe9cf20dd00dd44bf612
Wael Hcine <wael.hcine@esprit.tn>
2024-10-17 00:29:28 +0100

    aaaa

--- Vis/app.py
+++ Vis/app.py
@@ -1,5 +1,8 @@
 import streamlit as st
-
+import sys
+# Set the path to the 'fpl_utils' directory
+fpl_utils_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'fpl_utils')
+sys.path.append(fpl_utils_path)
 # Project Description
 st.title("Fantasy Premier League Analysis and Insights ")
 st.markdown("""
