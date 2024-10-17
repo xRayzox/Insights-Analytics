3ecfedb3caf00038f804d0863467e64ebcd7d446
Wael Hcine <wael.hcine@esprit.tn>
2024-10-17 16:18:38 +0100

    aazaz

--- Vis/pages/saa.py
+++ Vis/pages/saa.py
@@ -12,7 +12,13 @@ from fpl_api_collection import (
     get_fixt_dfs,
     get_league_table
 )
-
+from fpl_utils import (
+    define_sidebar,
+    get_annot_size,
+    map_float_to_color,
+    get_text_color_from_hash,
+    get_rotation
+)
 team_fdr_df, team_fixt_df, team_ga_df, team_gf_df = get_fixt_dfs()
 events_df = pd.DataFrame(get_bootstrap_data()['events'])
 
@@ -37,6 +43,9 @@ fdr_matrix = fdr_matrix.melt(id_vars='Team', var_name='GameWeek', value_name='FD
 # Convert FDR values to integers
 fdr_matrix['FDR'] = fdr_matrix['FDR'].astype(int)
 
+slider1, slider2 = st.slider('Gameweek: ', gw_min, gw_max, [int(ct_gw), int(ct_gw+4)], 1)
+annot_size = get_annot_size(slider1, slider2)
+rotation = get_rotation(slider1, slider2)
 # Define the custom color mapping for FDR values
 fdr_colors = {
     1: ("#257d5a", "black"),
