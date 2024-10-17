6ef55bdba2105c9ff6d07f9851c6ab3171b890ca
Wael Hcine <wael.hcine@esprit.tn>
2024-10-17 17:07:18 +0100

    zdadazdazdazdaz

--- Vis/pages/saa.py
+++ Vis/pages/saa.py
@@ -103,14 +103,14 @@ def get_selected_data(metric):
         return pivot_fdr_matrix
     elif metric == "Average Goals Against (GA)":
         ga_matrix = ga.melt(id_vars='Team', var_name='GameWeek', value_name='GA')
-        ga_matrix['GA'] = ga_matrix['GA'].astype(float) 
+        ga_matrix['GA'] = ga_matrix['GA'].astype(float).round(2)  # Round to 2 decimal places
         filtered_ga_matrix = ga_matrix[(ga_matrix['GameWeek'] >= slider1) & (ga_matrix['GameWeek'] <= slider2)]
         pivot_ga_matrix = filtered_ga_matrix.pivot(index='Team', columns='GameWeek', values='GA')
         pivot_ga_matrix.columns = [f'GW {col}' for col in pivot_ga_matrix.columns]
         return pivot_ga_matrix
     elif metric == "Average Goals For (GF)":
         gf_matrix = gf.melt(id_vars='Team', var_name='GameWeek', value_name='GF')
-        gf_matrix['GF'] = gf_matrix['GF'].astype(float)  
+        gf_matrix['GF'] = gf_matrix['GF'].astype(float).round(2)  # Round to 2 decimal places
         filtered_gf_matrix = gf_matrix[(gf_matrix['GameWeek'] >= slider1) & (gf_matrix['GameWeek'] <= slider2)]
         pivot_gf_matrix = filtered_gf_matrix.pivot(index='Team', columns='GameWeek', values='GF') 
         pivot_gf_matrix.columns = [f'GW {col}' for col in pivot_gf_matrix.columns]
