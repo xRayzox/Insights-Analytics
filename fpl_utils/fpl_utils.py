19b9c43eae3ea93358a077d7a767ffb302292416
Wael Hcine <wael.hcine@esprit.tn>
2024-10-17 01:23:18 +0100

    wawawawaw

--- fpl_utils/fpl_utils.py
+++ fpl_utils/fpl_utils.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import sys
+import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 from fpl_utils.fpl_api_collection import get_total_fpl_players
 import requests
