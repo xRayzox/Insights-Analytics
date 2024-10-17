1495ee0800e67d25e6148e83c53ba8357f8d95c7
Wael Hcine <wael.hcine@esprit.tn>
2024-10-17 23:11:06 +0100

    zadazdaz

--- Vis/pages/🏅_PL Table.py
+++ Vis/pages/🏅_PL Table.py
@@ -161,13 +161,13 @@ df_col, chart_col = st.columns([24, 24])  # Adjust the column sizes as needed
 # Configure progress columns for ratings
 column_config = {
     "ovr_rating" + ("_" + model_type if model_type else ""): st.column_config.ProgressColumn(
-        label="Overall Rating", max_value=max_ovr
+        label="Overall Rating", max_value=max_ovr,format="%.2f"
     ),
     "o_rating" + ("_" + model_type if model_type else ""): st.column_config.ProgressColumn(
-        label="Offensive Rating", max_value=max_o
+        label="Offensive Rating", max_value=max_o,format="%.2f"
     ),
     "d_rating" + ("_" + model_type if model_type else ""): st.column_config.ProgressColumn(
-        label="Defensive Rating", max_value=max_d
+        label="Defensive Rating", max_value=max_d,format="%.2f"
     ),
 }
 
