a3a1d92a10b059b317aa0f99b2651d569e90520a
Wael Hcine <wael.hcine@esprit.tn>
2024-10-17 16:20:16 +0100

    pulllllga

--- Vis/pages/saa.py
+++ Vis/pages/saa.py
@@ -10,15 +10,16 @@ from fpl_api_collection import (
     get_bootstrap_data,
     get_current_gw,
     get_fixt_dfs,
-    get_league_table
 )
 from fpl_utils import (
     define_sidebar,
     get_annot_size,
     map_float_to_color,
     get_text_color_from_hash,
-    get_rotation
+    get_rotation,
 )
+
+# Load data
 team_fdr_df, team_fixt_df, team_ga_df, team_gf_df = get_fixt_dfs()
 events_df = pd.DataFrame(get_bootstrap_data()['events'])
 
@@ -43,9 +44,18 @@ fdr_matrix = fdr_matrix.melt(id_vars='Team', var_name='GameWeek', value_name='FD
 # Convert FDR values to integers
 fdr_matrix['FDR'] = fdr_matrix['FDR'].astype(int)
 
-slider1, slider2 = st.slider('Gameweek: ', gw_min, gw_max, [int(ct_gw), int(ct_gw+4)], 1)
-annot_size = get_annot_size(slider1, slider2)
-rotation = get_rotation(slider1, slider2)
+# Create sliders for game week selection
+slider1, slider2 = st.slider('Gameweek: ', gw_min, gw_max, [int(ct_gw), int(ct_gw + 4)], 1)
+
+# Filter FDR matrix based on selected game weeks
+filtered_fdr_matrix = fdr_matrix[(fdr_matrix['GameWeek'] >= slider1) & (fdr_matrix['GameWeek'] <= slider2)]
+
+# Pivot the filtered FDR matrix for styling
+pivot_fdr_matrix = filtered_fdr_matrix.pivot(index='Team', columns='GameWeek', values='FDR')
+
+# Rename columns for display purposes
+pivot_fdr_matrix.columns = [f'GW {col}' for col in pivot_fdr_matrix.columns]
+
 # Define the custom color mapping for FDR values
 fdr_colors = {
     1: ("#257d5a", "black"),
@@ -63,14 +73,8 @@ def color_fdr(value):
     else:
         return ''  # No style for undefined values
 
-# Create a filtered FDR matrix for styling
-filtered_fdr_matrix = fdr_matrix.pivot(index='Team', columns='GameWeek', values='FDR')
-
-# Rename columns for display purposes
-filtered_fdr_matrix.columns = [f'GW {col}' for col in filtered_fdr_matrix.columns]
-
-# Apply the styling to the filtered FDR matrix
-styled_filtered_fdr_table = filtered_fdr_matrix.style.applymap(color_fdr)
+# Apply the styling to the pivoted FDR matrix
+styled_filtered_fdr_table = pivot_fdr_matrix.style.applymap(color_fdr)
 
 # Streamlit app to display the styled table
 st.title("Fixture Difficulty Rating (FDR) Matrix")
