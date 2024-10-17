a94392cb1b4bb46070d0b841107e97011bd70c63
Wael Hcine <wael.hcine@esprit.tn>
2024-10-17 23:43:06 +0100

    dazd

--- Vis/pages/🗓️_Fixtures.py
+++ Vis/pages/🗓️_Fixtures.py
@@ -5,6 +5,7 @@ import sys
 import numpy as np
 from datetime import datetime, timezone
 import streamlit.components.v1 as components
+import pytz
 
 # Adjust the path to your FPL API collection as necessary
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'FPL')))
