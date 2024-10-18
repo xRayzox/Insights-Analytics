0a0975c7eaa95afb2418d466c8e173d6829300e9
Wael Hcine <wael.hcine@esprit.tn>
2024-10-18 23:52:01 +0100

    dzadazd

--- Vis/pages/suiiii.py
+++ Vis/pages/suiiii.py
@@ -234,7 +234,7 @@ if fpl_id and gw_complete_list:
 
         # Define placements for each position zone
         zone_height = pitch_length / 6  # Height for each zone
-        space_between_zones = 1  # Space between zones
+        space_between_zones = 2  # Space between zones
 
         # Position calculations
         positions = {
