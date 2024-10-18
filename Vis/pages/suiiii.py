40dd71c37f7bef92d6a391aa78fc2da6be86d598
Wael Hcine <wael.hcine@esprit.tn>
2024-10-18 15:01:53 +0100

    azazdazddazd

--- Vis/pages/suiiii.py
+++ Vis/pages/suiiii.py
@@ -192,7 +192,7 @@ with col2:
         # Separate players into lineup and bench
         lineup = manager_team_df[manager_team_df['Played'] == True]
         bench = manager_team_df[manager_team_df['Played'] == False]
-
+        lineup
         # Define positions
         positions = {
             'GKP': [1, 5],
@@ -206,7 +206,7 @@ with col2:
         plt.plot([0, 0, 1, 1, 0], [0, 1, 1, 0, 0], color="black")  # Pitch outline
         plt.xlim(0, 1)
         plt.ylim(0, 1)
-
+"""
         # Plot players
         for index, row in lineup.iterrows():
             pos = row['Pos']
@@ -229,7 +229,7 @@ with col2:
 
         # Show the plot
         plt.show()
-
+"""
 
 ###############################################################################################################
 with col3:
