67a7db1ac31b14fee89d4dc0a6f4236ee1747ade
Wael Hcine <wael.hcine@esprit.tn>
2024-10-19 19:49:02 +0100

    azdazdaz

--- Vis/pages/suiiii.py
+++ Vis/pages/suiiii.py
@@ -260,6 +260,13 @@ if fpl_id and gw_complete_list:
             new_size = (100, 100)  # Adjust size as needed
             image = image.resize(new_size, Image.Resampling.LANCZOS)
 
+            # Create a circular mask
+            mask = Image.new("L", new_size, 0)
+
+
+            # Apply the mask to the image to make it round and transparent background
+            image = ImageOps.fit(image, mask.size, centering=(0.5, 0.5))
+            image.putalpha(mask)
             pos = row['Pos']
             if pos == 'GKP':
                 y_image = positions['GKP']
