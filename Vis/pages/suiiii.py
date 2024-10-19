a2db602bd6f3d00818bd5d340a62897e60749e91
Wael Hcine <wael.hcine@esprit.tn>
2024-10-19 21:19:14 +0100

    dazdaz

--- Vis/pages/suiiii.py
+++ Vis/pages/suiiii.py
@@ -404,7 +404,7 @@ if fpl_id and gw_complete_list:
 
             # Position for the GWP rectangle
             gwp_rect_x = x_bench - gwp_rect_width / 2  # Center the rectangle
-            gwp_rect_y = y_bench - rect_height - 5 - gwp_rect_height - 2  # Adjust the y position to place it below the first rectangle
+            gwp_rect_y = y_bench - rect_height - 5 - gwp_rect_height - 1  # Adjust the y position to place it below the first rectangle
 
             # Draw the GWP rectangle
             gwp_rect = FancyBboxPatch(
