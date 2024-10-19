36c34ff2c04f6af692352fbc09105aee29c7e1bc
Wael Hcine <wael.hcine@esprit.tn>
2024-10-19 21:04:14 +0100

    dazdaz

--- Vis/pages/suiiii.py
+++ Vis/pages/suiiii.py
@@ -305,7 +305,7 @@ if fpl_id and gw_complete_list:
             gwp_rect_height = 1  # Height for the GWP rectangle
 
             gwp_rect = FancyBboxPatch(
-                (x_image - rect_width / 2, gwp_rect_y-1),  # Center the rectangle
+                (x_image - rect_width / 2, gwp_rect_y+1),  # Center the rectangle
                 rect_width,
                 gwp_rect_height,
                 facecolor=(55/255, 0/255, 60/255),  # RGB color specified as a tuple normalized to [0, 1]
