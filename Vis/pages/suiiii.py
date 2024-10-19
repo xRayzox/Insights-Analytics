f09b68cd33608df344adef6c20c7624823ed90ba
Wael Hcine <wael.hcine@esprit.tn>
2024-10-19 20:53:26 +0100

    dazdaz

--- Vis/pages/suiiii.py
+++ Vis/pages/suiiii.py
@@ -310,7 +310,7 @@ if fpl_id and gw_complete_list:
 
             # Create a rounded rectangle patch at the correct location
             rounded_rect = FancyBboxPatch(
-                (x_image - rect_width / 2, y_image - rect_height - 7),  # Center the rectangle
+                (x_image - rect_width / 2, y_image - rect_height - 3),  # Center the rectangle
                 rect_width,
                 rect_height,
                 facecolor='white',
