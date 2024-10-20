89e4a1d9a55f8e0b17d5c9a4abe377e3238ef40e
Wael Hcine <wael.hcine@esprit.tn>
2024-10-20 01:57:01 +0100

    dazd

--- FPL/fpl_league.py
+++ FPL/fpl_league.py
@@ -1,13 +1,10 @@
 import requests
 import pandas as pd
 from fpl_api_collection import get_total_fpl_players
-import streamlit as st
+
 # Base URL for FPL API
 base_url = 'https://fantasy.premierleague.com/api/'
 
-# Define cache TTL and max size
-cache_maxsize = 128  # Maximum number of cache entries
-cache_ttl = 120  # Time-to-Live in seconds
 
 def entry_from_standings(standings):
     """Extract relevant information from league standings."""
@@ -48,25 +45,3 @@ def get_bootstrap_data():
     """Fetch static data that includes all players and game information."""
     r = requests.get(base_url + "bootstrap-static/").json()
     return r
-
-def get_names_managers():
-    total_players = get_total_fpl_players()  # Assuming this function returns a list of players
-    managers_list = []  # Initialize an empty list to hold manager data
-
-    for player in total_players:
-        try:
-            man_data = get_manager_details(player['id'])  # Get manager details for the player using their ID
-            curr_df = pd.DataFrame({
-                'id': [man_data['id']],  # Add manager ID to the DataFrame
-                'Manager': [f"{man_data['player_first_name']} {man_data['player_last_name']}"]  # Add manager name
-            })
-
-            managers_list.append(curr_df)  # Add the current DataFrame to the list
-        except Exception as e:
-            print(f"Error fetching manager details for player ID {player['id']}: {e}")
-
-    # Concatenate all individual DataFrames into a single DataFrame
-    if managers_list:
-        return pd.concat(managers_list, ignore_index=True)  # Return a single DataFrame of managers
-    else:
-        return pd.DataFrame(columns=['id', 'Manager'])  # Return an empty DataFrame if no managers found
