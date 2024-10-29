1824406be47a1d57c72bf8534dd8db2e20995838
Wael Hcine <wael.hcine@esprit.tn>
2024-10-29 14:52:38 +0100

    azddazdaz

--- Vis/pages/s.py
+++ Vis/pages/s.py
@@ -1,130 +1,224 @@
 import streamlit as st
+from pathlib import Path
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import urllib.request
+from PIL import Image
+from matplotlib.colors import LinearSegmentedColormap
+
+from plottable import ColumnDefinition, Table
+from plottable.cmap import normed_cmap
+from plottable.formatters import decimal_to_percent
+from plottable.plots import circled_image
+from plottable.plots import image
 import sys
 import os
-import altair as alt
-from matplotlib import pyplot as plt
-from mplsoccer import Bumpy
-from highlight_text import fig_text
-from matplotlib.offsetbox import OffsetImage
-import numpy as np
-
 pd.set_option('future.no_silent_downcasting', True)
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..','..', 'FPL')))
+
+# Adjust the path to include the FPL directory (assuming it's one level up)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'FPL')))
+
+# Now you can import your modules
 from fpl_api_collection import (
-    get_league_table, get_current_gw, get_fixt_dfs, get_bootstrap_data,calculate_points_by_weeks
+    get_league_table, get_current_gw, get_fixt_dfs, get_bootstrap_data
 )
 from fpl_utils import (
     define_sidebar
 )
-st.set_page_config(page_title='PL Table', page_icon=':sports-medal:', layout='wide')
-define_sidebar()
-st.title('Premier League Table')
 
+# --- Streamlit Configuration ---
+st.set_page_config(layout="wide")  # Use wide layout for better table visualization
+
+# --- Functions ---
+def load_image_from_url(url):
+    with urllib.request.urlopen(url) as response:
+        image = Image.open(response).convert("RGBA")
+    return image
+
+# --- Data Loading and Processing ---
 league_df = get_league_table()
-
 team_fdr_df, team_fixt_df, team_ga_df, team_gf_df = get_fixt_dfs()
-
 ct_gw = get_current_gw()
 
 new_fixt_df = team_fixt_df.loc[:, ct_gw:(ct_gw+2)]
 new_fixt_cols = ['GW' + str(col) for col in new_fixt_df.columns.tolist()]
 new_fixt_df.columns = new_fixt_cols
 
-new_fdr_df = team_fdr_df.loc[:, ct_gw:(ct_gw+2)]
-
 league_df = league_df.join(new_fixt_df)
-
 float_cols = league_df.select_dtypes(include='float64').columns.values
-
 league_df = league_df.reset_index()
 league_df.rename(columns={'team': 'Team'}, inplace=True)
 league_df.index += 1
-
 league_df['GD'] = league_df['GD'].map('{:+}'.format)
 
 teams_df = pd.DataFrame(get_bootstrap_data()['teams'])
-teams_df['logo_url'] = "https://resources.premierleague.com/premierleague/badges/70/t" + teams_df['code'].astype(str) + ".png"
-team_logo_mapping = pd.Series(teams_df.logo_url.values, index=teams_df.short_name).to_dict()
-
+teams_df['logo_url'] = "https://resources.premierleague.com/premierleague/badges/70/t" + teams_df['code'].astype(str) + "@x2.png"
+teams_df['logo_image'] = teams_df['logo_url'].apply(load_image_from_url)
+team_logo_mapping = pd.Series(teams_df['logo_image'].values, index=teams_df['short_name']).to_dict()
 league_df['logo_team'] = league_df['Team'].map(team_logo_mapping)
+league_df['Rank'] = league_df['Pts'].rank(ascending=False, method='min').astype(int)
+league_df['Form'] = league_df['form'].str.replace('W', ' ', regex=False).str.replace('L', ' ', regex=False).str.replace('D', ' ', regex=False)
 
-st.write(league_df)
+# --- Streamlit App ---
+st.title("Premier League Table")
 
-st.write(team_logo_mapping)
-## Very slow to load, works but needs to be sped up.
-def get_home_away_str_dict():
-    new_fdr_df.columns = new_fixt_cols
-    result_dict = {}
-    for col in new_fdr_df.columns:
-        values = list(new_fdr_df[col])
-        max_length = new_fixt_df[col].str.len().max()
-        if max_length > 7:
-            new_fixt_df.loc[new_fixt_df[col].str.len() <= 7, col] = new_fixt_df[col].str.pad(width=max_length+9, side='both', fillchar=' ')
-        strings = list(new_fixt_df[col])
-        value_dict = {}
-        for value, string in zip(values, strings):
-            if value not in value_dict:
-                value_dict[value] = []
-            value_dict[value].append(string)
-        result_dict[col] = value_dict
-    
-    merged_dict = {}
-    merged_dict[1.5] = []
-    merged_dict[2.5] = []
-    merged_dict[3.5] = []
-    merged_dict[4.5] = []
-    for k, dict1 in result_dict.items():
-        for key, value in dict1.items():
-            if key in merged_dict:
-                merged_dict[key].extend(value)
-            else:
-                merged_dict[key] = value
-    for k, v in merged_dict.items():
-        decoupled_list = list(set(v))
-        merged_dict[k] = decoupled_list
-    for i in range(1,6):
-        if i not in merged_dict:
-            merged_dict[i] = []
-    return merged_dict
+# --- Table Styling ---
+bg_color = "#FFFFFF"
+text_color = "#000000"
 
+row_colors = {
+    "top4": "#E1FABC",
+    "top6": "#FFFC97",
+    "relegation": "#E79A9A",
+    "even": "#E2E2E1",
+    "odd": "#B3B0B0"
+}
 
-home_away_dict = get_home_away_str_dict()
+matplotlib.rcParams["text.color"] = text_color
+matplotlib.rcParams["font.family"] = "monospace"
 
-def color_fixtures(val):
-    bg_color = 'background-color: '
-    font_color = 'color: '
-    if val in home_away_dict[1]:
-        bg_color += '#147d1b'
-    if val in home_away_dict[1.5]:
-        bg_color += '#0ABE4A'
-    elif val in home_away_dict[2]:
-        bg_color += '#00ff78'
-    elif val in home_away_dict[2.5]:
-        bg_color += "#caf4bd"
-    elif val in home_away_dict[3]:
-        bg_color += '#eceae6'
-    elif val in home_away_dict[3.5]:
-        bg_color += "#fa8072"
-    elif val in home_away_dict[4]:
-        bg_color += '#ff0057'
-        font_color += 'white'
-    elif val in home_away_dict[4.5]:
-        bg_color += '#C9054F'
-        font_color += 'white'
-    elif val in home_away_dict[5]:
-        bg_color += '#920947'
-        font_color += 'white'
-    else:
-        bg_color += ''
-    style = bg_color + '; ' + font_color
-    return style
+# --- Column Definitions ---
+col_defs = [
+    ColumnDefinition(
+        name="Rank",
+        textprops={'ha': "center"},
+        width=0.5
+    ),
+    ColumnDefinition(
+        name="logo_team",
+        textprops={'ha': "center", 'va': "center", 'color': "white"},
+        plot_fn=image,
+        width=0.5,
+    ),
+    ColumnDefinition(
+        name="Team",
+        textprops={'ha': "left", 'weight': "bold"},
+        width=1.75
+    ),
+    ColumnDefinition(
+        name="GP",
+        group="Matches Played",
+        textprops={'ha': "center"},
+        width=0.5
+    ),
+    ColumnDefinition(
+        name="W",
+        group="Matches Played",
+        textprops={'ha': "center"},
+        width=0.5
+    ),
+    ColumnDefinition(
+        name="D",
+        group="Matches Played",
+        textprops={'ha': "center"},
+        width=0.5
+    ),
+    ColumnDefinition(
+        name="L",
+        group="Matches Played",
+        textprops={'ha': "center"},
+        width=0.5
+    ),
+    ColumnDefinition(
+        name="GF",
+        group="Goals",
+        textprops={'ha': "left", 'weight': "bold"},
+        width=0.5
+    ),
+    ColumnDefinition(
+        name="GA",
+        group="Goals",
+        textprops={'ha': "left", 'weight': "bold"},
+        width=0.5
+    ),
+    ColumnDefinition(
+        name="GD",
+        group="Goals",
+        textprops={'ha': "left", 'weight': "bold"},
+        width=0.5
+    ),
+    ColumnDefinition(
+        name="CS",
+        group="Goals",
+        textprops={'ha': "left", 'weight': "bold"},
+        width=0.5
+    ),
+    ColumnDefinition(
+        name="Pts",
+        group="Points",
+        textprops={'ha': "left", 'weight': "bold"},
+        width=1
+    ),
+    ColumnDefinition(
+        name="Pts/Game",
+        group="Points",
+        textprops={'ha': "left", 'weight': "bold"},
+        width=1
+    ),
+    ColumnDefinition(
+        name="Form",
+        group="Points",
+        textprops={'ha': "left", 'weight': "bold"},
+        width=1
+    ),
+    ColumnDefinition(
+        name="GF/Game",
+        group="ByGame",
+        textprops={'ha': "left", 'weight': "bold"},
+        width=1
+    ),
+    ColumnDefinition(
+        name="GA/Game",
+        group="ByGame",
+        textprops={'ha': "left", 'weight': "bold"},
+        width=1
+    ),
+    ColumnDefinition(
+        name="CS/Game",
+        group="ByGame",
+        textprops={'ha': "left", 'weight': "bold"},
+        width=1
+    ),
+    ColumnDefinition(
+        name="GW10",
+        group="Fixtures",
+        textprops={'ha': "left", 'weight': "bold"},
+        width=1
+    ),
+    ColumnDefinition(
+        name="GW11",
+        group="Fixtures",
+        textprops={'ha': "left", 'weight': "bold"},
+        width=1
+    ),
+    ColumnDefinition(
+        name="GW12",
+        group="Fixtures",
+        textprops={'ha': "left", 'weight': "bold"},
+        width=1
+    )
+]
+# --- Plottable Table ---
+fig, ax = plt.subplots(figsize=(16, 10))  # Adjust figsize for Streamlit
+fig.set_facecolor(bg_color)
+ax.set_facecolor(bg_color)
 
-for col in new_fixt_cols:
-    if league_df[col].dtype == 'O':
-        max_length = league_df[col].str.len().max()
-        if max_length > 7:
-            league_df.loc[league_df[col].str.len() <= 7, col] = league_df[col].str.pad(width=max_length+9, side='both', fillchar=' ')
+table = Table(
+    league_df,
+    column_definitions=col_defs,
+    columns=['Team', 'logo_team', 'GP', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'CS', 'Pts', 'Form',
+             'Pts/Game', 'GF/Game', 'GA/Game', 'CS/Game', 'GW10', 'GW11', 'GW12', 'Rank'], 
+    index_col="Rank",
+    row_dividers=True,
+    row_divider_kw={"linewidth": 1, "linestyle": (0, (1, 5))},
+    footer_divider=True,
+    textprops={"fontsize": 12},  # Adjust fontsize for Streamlit
+    col_label_divider_kw={"linewidth": 1, "linestyle": "-"},
+    column_border_kw={"linewidth": .5, "linestyle": "-"},
+    ax=ax
+)
 
-st.dataframe(league_df.style.applymap(color_fixtures, subset=new_fixt_cols) \
-             .format(subset=float_cols, formatter='{:.2f}'), height=740, width=None)
+# --- Display the Table in Streamlit ---
+st.pyplot(fig)
