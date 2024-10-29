5f49b5810b978c568cd7fb027e2a3ff4300e647f
Wael Hcine <wael.hcine@esprit.tn>
2024-10-29 17:53:35 +0100

    dazdazd

--- Vis/pages/s.py
+++ Vis/pages/s.py
@@ -312,10 +312,6 @@ table = Table(
     ax=ax
 )
 
-for idx in range(len(league_df)):
-    gw_value = league_df.iloc[idx][f'GW{ct_gw+1}']
-    color = color_fixtures(gw_value)
-    table.rows[idx].cells[-2].set_facecolor(color)  # Assuming GW{ct_gw+1} is the second to last cell in the row
 
 for idx in range(len(league_df)):
     if league_df.iloc[idx]['Rank'] <= 4:
