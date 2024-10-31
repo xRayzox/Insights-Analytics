78508f9f24f8949ec5419f0ed36574b9fb206734
Wael Hcine <wael.hcine@esprit.tn>
2024-10-31 16:35:38 +0100

    dzadad

--- Vis/pages/🏅_PL Table.py
+++ Vis/pages/🏅_PL Table.py
@@ -351,35 +351,6 @@ max_d = rating_df["d_rating" + ("_" + model_type if model_type else "")].max()
 # Set up columns for layout
 df_col, chart_col = st.columns([24, 24])  # Adjust the column sizes as needed
 
-
-
-
-
-# Dynamically select columns for display
-columns_to_display = ["name",
-                       "ovr_rating" + ("_" + model_type if model_type else ""),
-                       "o_rating" + ("_" + model_type if model_type else ""),
-                       "d_rating" + ("_" + model_type if model_type else "")]
-
-
-
-with df_col:
-    st.dataframe(
-        rating_df[columns_to_display],
-        hide_index=True,
-        use_container_width=True,
-        column_config={
-    "ovr_rating" + ("_" + model_type if model_type else ""): st.column_config.ProgressColumn(
-        label="Overall Rating", max_value=max_ovr, format="%.0f"
-    ),
-    "o_rating" + ("_" + model_type if model_type else ""): st.column_config.ProgressColumn(
-        label="Offensive Rating", max_value=max_o, format="%.0f"
-    ),
-    "d_rating" + ("_" + model_type if model_type else ""): st.column_config.ProgressColumn(
-        label="Defensive Rating", max_value=max_d, format="%.0f"
-    ),
-}
-    )
 # Scatter plot setup
 x_domain = [teams_df["d_rating" + ("_" + model_type if model_type else "")].min()-0.1, teams_df["d_rating" + ("_" + model_type if model_type else "")].max() + 0.1]
 y_range = [teams_df["o_rating" + ("_" + model_type if model_type else "")].min()-100, teams_df["o_rating" + ("_" + model_type if model_type else "")].max() + 100]
