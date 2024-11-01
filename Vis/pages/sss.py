9b19479e41630076766ed6983880fe0a015dae20
Wael Hcine <wael.hcine@esprit.tn>
2024-11-01 16:32:51 +0100

    dazdazda

--- Vis/pages/sss.py
+++ Vis/pages/sss.py
@@ -264,7 +264,7 @@ from mplsoccer import Radar, grid
 def plot_position_radar(df_player, name):
     # Ensure the DataFrame is reset to avoid index issues
     df_player.reset_index(drop=True)
-    df_player.drop('web_name')
+    df_player.drop('web_name', axis=1, inplace=True)
     element_type = df_player["element_type"].iloc[0] 
     
     # Define columns and fields based on player position
