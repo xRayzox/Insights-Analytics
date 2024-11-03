dd91382ce8095c91780ab106c972026079f03c10
Wael Hcine <wael.hcine@esprit.tn>
2024-11-03 22:04:42 +0100

    azdazda

--- Vis/pages/sss_final.py
+++ Vis/pages/sss_final.py
@@ -306,8 +306,9 @@ def plot_position_radar(df_player,name,df_player1,name1):
     element_type = df_player["element_type"].iloc[0]
     df_player1.reset_index(drop=True, inplace=True)
     df_player1['TSB%'] = df_player1['TSB%'] * 100
-    st.write(df_player1['team'])
+    
     df = ele_copy.copy()
+    st.write(df)
     
     # Define column names and labels based on player position
     if element_type == 'GKP':
