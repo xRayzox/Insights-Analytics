4915eb25d2330c6ceb0d7e82e234d9bcf97521d9
Wael Hcine <wael.hcine@esprit.tn>
2024-11-03 21:45:33 +0100

    dazdazd

--- Vis/pages/sss_final.py
+++ Vis/pages/sss_final.py
@@ -639,6 +639,6 @@ df_player1=collated_spider_df_from_name(player1)
 df_player2=collated_spider_df_from_name(player2)
 
 figg=plot_position_radar(df_player1,player1,df_player2,player2)
-clo=st.columns([3,5,3])
+clo=st.columns([1,8,1])
 with clo[1]:
     st.write(figg)
