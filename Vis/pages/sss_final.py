36d88de44d7fa4ebc023b83f7d96a9ca3c80c745
Wael Hcine <wael.hcine@esprit.tn>
2024-11-03 20:46:48 +0100

    dazda

--- Vis/pages/sss_final.py
+++ Vis/pages/sss_final.py
@@ -538,12 +538,10 @@ else:
                         .columns.values, formatter='{:.2f}')
         init_rows[1].dataframe(styled_player1_next3)  
 
-        element_type_for_player1 = ele_cut.loc[ele_cut['full_name'] == player1, 'element_type'].iloc[0]
-        ele_cut_copy= ele_cut[ele_cut['element_type'] == element_type_for_player1].copy()
-        id_dict1 = dict(zip(ele_cut_copy['id'], ele_cut_copy['full_name']))
-        if len(id_dict1) > 1:
+        
+        if len(id_dict) > 1:
                
-            player2 = init_rows[2].selectbox("Choose Player Two", id_dict1.values(), 1) #index=int(ind2))
+            player2 = init_rows[2].selectbox("Choose Player Two", id_dict.values(), 1) #index=int(ind2))
             player2_next3 = get_player_next3(player2)
             for col in new_fixt_cols:
                 if player2_next3[col].dtype == 'O':
