ada06181717bd959d02fee823a393ade25781bef
Wael Hcine <wael.hcine@esprit.tn>
2024-11-03 20:38:36 +0100

    azdada

--- Vis/pages/sss_final.py
+++ Vis/pages/sss_final.py
@@ -558,15 +558,12 @@ else:
         player1_total_df = collate_total_df_from_name(player1)
         player1_total_df.drop(['team', 'element_type'], axis=1, inplace=True)
         rows[0].dataframe(player1_df.style.format({'Price': '£{:.1f}'}))
-        total_fmt = {'xG':'{:.2f}', 'xA':'{:.2f}', 'xGI':'{:.2f}', 'xGC':'{:.2f}',
-                     'Price': '£{:.1f}', 'TSB%': '{:.1%}'}
+
         
         if len(id_dict) > 1:
             player2_df = collate_hist_df_from_name(player2)
             player2_total_df = collate_total_df_from_name(player2)
             player2_total_df.drop(['team', 'element_type'], axis=1, inplace=True)
             rows[1].dataframe(player2_df.style.format({'Price': '£{:.1f}'}))
-            total_fmt = {'xG':'{:.2f}', 'xA':'{:.2f}', 'xGI':'{:.2f}', 'xGC':'{:.2f}',
-                     'Price': '£{:.1f}', 'TSB%': '{:.1%}'}
 
         
