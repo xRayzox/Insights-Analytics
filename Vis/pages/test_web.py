bf252aa4b51bfedafe440c1b8ae6457a2930c23d
Wael Hcine <wael.hcine@esprit.tn>
2024-11-04 02:19:25 +0100

    azdazd

--- Vis/pages/test_web.py
+++ Vis/pages/test_web.py
@@ -1,23 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
+
 import streamlit as st
-@st.cache_resource
-def get_driver():
-    return webdriver.Chrome(
-        service=Service(
-            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-        ),
-        options=options,
-    )
+import chromedriver_autoinstaller
 
-options = Options()
-options.add_argument("--disable-gpu")
-options.add_argument("--headless")
+chromedriver_autoinstaller.install()
 
-driver = get_driver()
+import warnings
+warnings.filterwarnings('ignore')
+driver = webdriver.Chrome()
+
 driver.get("https://www.whoscored.com/Matches/1821249/Live/England-Premier-League-2024-2025-Bournemouth-Manchester-City")
 
 st.code(driver.page_source)
