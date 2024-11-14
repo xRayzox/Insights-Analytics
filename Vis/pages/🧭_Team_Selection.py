66a03e600e5321c4250ef74fd071abb71d02bb64
Wael Hcine <wael.hcine@esprit.tn>
2024-11-14 15:03:39 +0100

    azdada

--- Vis/pages/🧭_Team_Selection.py
+++ Vis/pages/🧭_Team_Selection.py
@@ -13,6 +13,10 @@ from sklearn.model_selection import train_test_split, cross_val_score, KFold
 from sklearn.metrics import mean_squared_error
 import numpy as np
 import time
+
+
+
+
 cwd = os.getcwd()
 # Construct the full path to the 'FPL' directory
 fpl_path = os.path.join(cwd, 'FPL')
@@ -33,47 +37,41 @@ from fpl_api_collection import (
 start_time = time.time()
 
 
-# Retrieve and prepare player data
-ele_types_data = get_bootstrap_data()['element_types']
-ele_types_X_weighted = pd.DataFrame(ele_types_data)
-ele_data = get_bootstrap_data()['elements']
-ele_df = pd.DataFrame(ele_data)
-ele_df['element_type'] = ele_df['element_type'].map(ele_types_X_weighted.set_index('id')['singular_name_short'])
-ele_copy = ele_df.copy()
+# Retrieve and prepare data
+bootstrap_data = get_bootstrap_data()
+teams_df = pd.DataFrame(bootstrap_data['teams'])
+element_types_df = pd.DataFrame(bootstrap_data['element_types'])
+elements_df = pd.DataFrame(bootstrap_data['elements'])
 
-# Retrieve and prepare team data
-teams_data = get_bootstrap_data()['teams']
-teams_df = pd.DataFrame(teams_data)
-
-# Map team IDs to names for fixture processing
+# Map team IDs to names
 team_name_mapping = pd.Series(teams_df.name.values, index=teams_df.id).to_dict()
-ele_copy['team_name'] = ele_copy['team'].map(teams_df.set_index('id')['short_name'])
-ele_copy['full_name'] = ele_copy['first_name'].str.cat(ele_copy['second_name'].str.cat(ele_copy['team_name'].apply(lambda x: f" ({x})"), sep=''), sep=' ')
+
+# Prepare player data and map element types
+ele_copy = elements_df.assign(
+    element_type=lambda df: df['element_type'].map(element_types_df.set_index('id')['singular_name_short']),
+    team_name=lambda df: df['team'].map(teams_df.set_index('id')['short_name']),
+    full_name=lambda df: df['first_name'].str.cat(df['second_name'].str.cat(df['team_name'].apply(lambda x: f" ({x})"), sep=''), sep=' ')
+)
 
 # Retrieve player dictionary and current season/gameweek
 full_player_dict = get_player_id_dict('total_points', web_name=False)
-crnt_season = get_current_season()
-ct_gw = get_current_gw()
+crnt_season, ct_gw = get_current_season(), get_current_gw()
 
 # Retrieve and process fixture data
 fixture_data = get_fixture_data()
-fixtures_df = pd.DataFrame(fixture_data)
-fixtures_df.drop(columns='stats', inplace=True)
-
-fixtures_df['team_h'] = fixtures_df['team_h'].replace(team_name_mapping)
-fixtures_df['team_a'] = fixtures_df['team_a'].replace(team_name_mapping)
-fixtures_df = fixtures_df.drop(columns=['pulse_id'])
+fixtures_df = pd.DataFrame(fixture_data).drop(columns='stats').replace(
+    {'team_h': team_name_mapping, 'team_a': team_name_mapping}
+).drop(columns=['pulse_id'])
 
 # Format fixture dates
 timezone = 'Europe/London'
 fixtures_df['datetime'] = pd.to_datetime(fixtures_df['kickoff_time'], utc=True)
-fixtures_df['local_time'] = fixtures_df['datetime'].dt.tz_convert(timezone).dt.strftime('%A %d %B %Y %H:%M')
-fixtures_df['local_date'] = fixtures_df['datetime'].dt.tz_convert(timezone).dt.strftime('%d %A %B %Y')
-fixtures_df['local_hour'] = fixtures_df['datetime'].dt.tz_convert(timezone).dt.strftime('%H:%M')
+fixtures_df[['local_time', 'local_date', 'local_hour']] = fixtures_df['datetime'].dt.tz_convert(timezone).apply(
+    lambda x: pd.Series([x.strftime('%A %d %B %Y %H:%M'), x.strftime('%d %A %B %Y'), x.strftime('%H:%M')])
+)
 
 # Retrieve fixture difficulty rating data
 team_fdr_df, team_fixt_df, team_ga_df, team_gf_df = get_fixt_dfs()
-full_player_dict = get_player_id_dict('total_points', web_name=False)
 
 elapsed_time = time.time() - start_time
 st.error(f"1-Time taken by my_function: {elapsed_time} seconds")
