5a0095fc4ff5ea826c391c400d0a8181ea06ab40
Wael Hcine <wael.hcine@esprit.tn>
2024-11-17 17:41:55 +0100

    zadazda

--- Vis/pages/🧭_Team_Selection.py
+++ Vis/pages/🧭_Team_Selection.py
@@ -476,7 +476,7 @@ total_stats = pulga.groupby('Player')[columns_to_normalize + ['Combined_Weight']
     lambda x: (x[columns_to_normalize].multiply(x['Combined_Weight'], axis=0).sum()) / x['Combined_Weight'].sum()
 ).reset_index()
 
-
+total_stats[columns_to_normalize] = 0
 df_pred = pd.merge(fit, total_stats,
                            left_on='Player', right_on='Player', how='left')
 
