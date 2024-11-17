a6f3a4f3763996ac3c2bf976de96213ba47d465c
Wael Hcine <wael.hcine@esprit.tn>
2024-11-17 16:26:35 +0100

    azddaz

--- Vis/pages/🧭_Team_Selection.py
+++ Vis/pages/🧭_Team_Selection.py
@@ -851,7 +851,9 @@ def draw_players(df, positions, ax, pitch):
 
             # Draw the player image on the pitch
             pitch.inset_image(y_image, x_image, image, height=9, ax=ax)
-
+            pic=Image.open('./data/captain.png')
+            if row.Role== 'Captain':
+                st.write(pic)
             # Draw player's name and GWP points
             draw_player_details(ax, row, x_image, y_image)
 
