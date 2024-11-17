3654b5888b92de5ccf957cb9d6902344d1e562f0
Wael Hcine <wael.hcine@esprit.tn>
2024-11-17 16:21:53 +0100

    azdazd

--- Vis/pages/🧭_Team_Selection.py
+++ Vis/pages/🧭_Team_Selection.py
@@ -856,9 +856,9 @@ def draw_players(df, positions, ax, pitch):
             # Draw the player image on the pitch
             pitch.inset_image(y_image, x_image, image, height=9, ax=ax)
             
-
+            pic=load_image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngegg.com%2Fen%2Fpng-cbcyh&psig=AOvVaw0bMinkw1yxU2z9Wq_upbQP&ust=1731942075525000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIDxjKnS44kDFQAAAAAdAAAAABAE")
             if row.Role== 'Captain':
-                pitch.inset_image(x_image, y_image, load_image('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngegg.com%2Fen%2Fpng-cbcyh&psig=AOvVaw0bMinkw1yxU2z9Wq_upbQP&ust=1731942075525000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIDxjKnS44kDFQAAAAAdAAAAABAE'), height=4, ax=ax)  # Adjust height as needed
+                pitch.inset_image(x_image, y_image, pic, height=4, ax=ax)  # Adjust height as needed
 
             # Draw player's name and GWP points
             draw_player_details(ax, row, x_image, y_image)
