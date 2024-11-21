56a3c36003d6e373d60f85012bf2cab4e1875cff
Wael Hcine <wael.hcine@esprit.tn>
2024-11-21 12:57:06 +0100

    azdazd

--- Vis/pages/👔_Manager.py
+++ Vis/pages/👔_Manager.py
@@ -368,7 +368,7 @@ with col5:
                         # Draw the player image on the pitch
                         pitch.inset_image(y_image, x_image, image, height=9, ax=ax)
                         # Add captain badge if applicable
-                        if row['role'] == '(C)':
+                        if row.role == '(C)':
                             captain_icon = Image.open('./data/captain.png')
                             pitch.inset_image(y_image - 2, x_image - 3, captain_icon, height=2, ax=ax)
                         # Draw player's name and GWP points
